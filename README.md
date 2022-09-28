463226e954bbd6b9e5e815a5732e098cead72930
roqurdl <uyhnri@gmail.com>
2022-09-28 21:11:50 +0900

    coinCalculator

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 #Practice ReactJS with Create React App
 
+//toDoList Commmit
+
 toDoList 제작(in toDoList Commit)
 List Delete Buttton 제작에서
 localStorage 및 DB를 사용하지 않기에 parentElement를 사용 remove 시켜줌.
 위 경우 Title 옆 List 수를 나타내는 것에는 변화가 적용 X
 ==> List의 index를 받게하여 fliter 함수를 사용으로 해결(Commit toDoListUpdate에 적용)
+
+//CoinCalculate commit
--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 function App() {
-  const deletBtn = (index) => {
-    setDoList(doList.filter((todo, listIndex) => index !== listIndex));
-  };
-  const [toDo, setToDo] = useState("");
-  const [doList, setDoList] = useState([]);
+  const API_URL = `https://api.coinpaprika.com/v1/tickers`;
+
+  const [loading, setloading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [price, setPrice] = useState(1);
+  const [pay, setPay] = useState(1);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setloading(false);
+      });
+  });
   const onChange = (e) => {
-    setToDo(e.target.value);
+    setPay(e.target.value);
   };
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setDoList((curArray) => [...curArray, toDo]);
-    setToDo("");
+  const onSelect = (e) => {
+    setPrice(e.target.value);
+    setPay(1);
   };
   return (
     <div>
-      <h1>To Do List ({doList.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={toDo}
-          placeholder="Write To Do."
-          onChange={onChange}
-        ></input>
-        <button>Add List</button>
-      </form>
-      <hr />
-      <ul>
-        {doList.map((todo, index) => (
-          <li key={index}>
-            {todo}
-            <button onClick={() => deletBtn(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <h1>The Coin{loading ? null : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select id="coin" onChange={onSelect}>
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price} key={coin.id}>
+              {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <div>
+        <label htmlFor="pay">How much USD you have?</label>
+        <input type="number" id="pay" min="1" onChange={onChange} value={pay} />
+      </div>
+      <h2>You can get {pay / price}BTC</h2>
     </div>
   );
 }
